adfcb35b7dfd33fb1dfdfbbaea05bc6496180dab
UniverseSquared <universesquared4@gmail.com>
2024-11-16 15:19:37 +0000

    always spawn player on a point

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
-local screen_width = 640
-local screen_height = 480
+local screen_width = 800
+local screen_height = 600
 
-local player_x = 300
-local player_y = 300
+local player_x = screen_width / 2
+local player_y = screen_height / 2
+
+local player_scale = 0.1
 
 local vines = {} -- { from = {x, y}, to = {x, y} }
 local vine_point_radius = 10
@@ -15,11 +17,18 @@ local function generate_random_points(n)
         table.insert(points, { x, y })
     end
 
+    local centre_x = screen_width / 2 - vine_point_radius / 2
+    local centre_y = screen_height / 2 - vine_point_radius / 2
+    table.insert(points, { centre_x, centre_y })
+
     return points
 end
 
 function love.load()
     player_image = love.graphics.newImage("assets/main_character.png")
+    player_image_width = player_image:getWidth() * player_scale
+    player_image_height = player_image:getHeight() * player_scale
+
     vine_points = generate_random_points(50)
 end
 
@@ -68,6 +77,9 @@ function love.draw()
         love.graphics.circle("fill", point[1], point[2], vine_point_radius)
     end
 
+    local render_x = player_x - player_image_width / 2
+    local render_y = player_y - player_image_height / 2
+
     love.graphics.setColor(0.929, 0.266, 0.498)
-    love.graphics.draw(player_image, player_x, player_y, 0, 0.1, 0.1)
+    love.graphics.draw(player_image, render_x, render_y, 0, player_scale, player_scale)
 end
